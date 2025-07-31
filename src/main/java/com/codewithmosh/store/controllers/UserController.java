553b1ba8665befc4dbc310a965c911b8d2df1393
venelinpetrov <vpetrov33tu@gmail.com>
2025-07-31 19:19:06 +0300

    Init

---
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -0,0 +1,28 @@
+package com.codewithmosh.store.controllers;
+
+import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
