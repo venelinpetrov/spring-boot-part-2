2b72e482cac2d115f43529703f458c35191967ad
venelinpetrov <vpetrov33tu@gmail.com>
2025-08-01 15:45:10 +0300

    Add sort query parameter

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,17 +1,15 @@
 package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.UserDto;
-import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -21,8 +19,11 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll()
+    public Iterable<UserDto> getAllUsers(@RequestParam(required = false, defaultValue = "", name="sort") String sortBy) {
+        if (!Set.of("name", "email", "createdAt").contains(sortBy)) {
+            sortBy = "name";
+        }
+        return userRepository.findAll(Sort.by(sortBy))
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
@@ -35,8 +36,6 @@ public class UserController {
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
-
-        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
 
         return ResponseEntity.ok(userMapper.toDto(user));
     }
