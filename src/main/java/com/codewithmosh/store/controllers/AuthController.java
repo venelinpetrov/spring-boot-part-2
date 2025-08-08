03c767f265834c87496f15f72b717431c5de1b5c
venelinpetrov <vpetrov33tu@gmail.com>
2025-08-08 16:26:35 +0300

    Add DAO provider

--- src/main/java/com/codewithmosh/store/controllers/AuthController.java
+++ src/main/java/com/codewithmosh/store/controllers/AuthController.java
@@ -1,12 +1,11 @@
 package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.LoginDto;
-import com.codewithmosh.store.repositories.UserRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,20 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
-    private final PasswordEncoder passwordEncoder;
-    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
     public ResponseEntity<Void> login(@Valid @RequestBody LoginDto body) {
-        var user = userRepository.findByEmail(body.getEmail()).orElse(null);
-
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
-        if (!passwordEncoder.matches(body.getPassword(), user.getPassword())) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(body.getEmail(), body.getPassword())
+        );
 
         return ResponseEntity.ok().build();
     }
